8e20483b228a08986cd3e3a5e5bd04fcc04138c0
Victor Parmar <vic@smalldata.tech>
2019-09-16 11:48:36 +0200

    c update release script

--- release.sh
+++ release.sh
@@ -3,14 +3,14 @@
 usage () {
     cat <<EOM
 Usage:
-$(basename $0) [tag-name]
+$(basename $0) [tag-name] [deploy]
 
 EOM
 #    exit 0
 }
 
 print_command_header () {
-  printf "\n##\n# RELEASE: $1\n##\n"
+  printf "\n##\n# LIGHT-MESSENGER: $1\n##\n"
 }
 
 check_command_success () {
@@ -21,20 +21,13 @@ check_command_success () {
   fi
 }
 
-if [[ $(git diff --stat) != '' ]]; then
-  echo "modified / untracked files\n\n"
-  exit 1
-fi
+release_tag () {
+  tag=$1
 
-tag_arg=$1
-
-if [[ $tag_arg ]]
-then
   print_command_header "push changes"
   git push origin
   check_command_success "push"
   
-  tag=$tag_arg
   print_command_header "tagging $tag"
   git tag $tag
   check_command_success "creating git tag"
@@ -42,15 +35,9 @@ then
   print_command_header "pushing git tag"
   git push origin $tag
   check_command_success "pushing git tag"
-  
-  print_command_header "run tests"
-  make test
-  check_command_success "tests"
-  
-  print_command_header "build"
-  make build
-  check_command_success "build"
-  
+}
+
+release_deploy () {
   print_command_header "stop service"
   ssh nofasy@radmon 'sudo systemctl stop light-messenger'
   check_command_success "stop current service"
@@ -66,7 +53,44 @@ then
   print_command_header "status service"
   ssh nofasy@radmon 'sudo systemctl status light-messenger'
   check_command_success "status service"
-  
+}
+
+##
+# - check for any modified / untracked files
+# - check that all tests pass
+# - check release tag is provided and tag release if yes
+# - check if deploy tag is provided and deploy if yes
+##
+
+if [[ $(git diff --stat) != '' ]]; then
+  printf "modified / untracked files\n"
+  exit 1
+fi
+
+print_command_header "run tests"
+make test
+check_command_success "tests"
+
+arg_tag=$1
+arg_deploy=$2
+
+if [[ $arg_tag ]]
+then
+  release_tag "$arg_tag"
+
+  print_command_header "build"
+  make build
+  check_command_success "build"
+
+  if [[ $arg_deploy ]]
+  then
+    release_deploy
+  else
+    printf "No deploy tag provided\n"
+  fi
+
 else
   printf "require tag name\n"
 fi
+
+printf "done\n"
